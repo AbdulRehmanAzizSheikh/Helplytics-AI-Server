66888d398826a4edb72307224dd6f27b6ff7815a
abdulrehmanazizf5 <abdulrehmanaziz5f@gmail.com>
2026-04-19 01:02:54 +0500

    feat: add MongoDB integration and refactor server structure into app and config modules

--- index.js
+++ index.js
@@ -1,18 +1,10 @@
-import express from "express";
-import cors from "cors";
 import dotenv from "dotenv";
+import app from "./src/app.js";
+import connectDB from "./src/config/db.js";
 
 dotenv.config();
 
-const app = express();
-
-app.use(cors());
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
+  connectDB();
 });
